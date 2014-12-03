3cb1e0bd5162f0625bf65b8cba1fc50cb6122c91
Erik Madsen <erik@madsense.net>
2014-12-03 15:22:39 -0500

    grab noshows db version

--- webviewer/getnoshows.php
+++ webviewer/getnoshows.php
@@ -6,7 +6,7 @@ $group_id = $_GET['group_id'];
 
 if (!empty ($group_id)) {
 
-    $query_string = sprintf("SELECT n.group_id, n.header_id, h.mjd, h.ra_deg, h.dec_deg FROM noshows as n LEFT JOIN headers as h ON n.header_id=h.header_id AND n.db_version=h.db_version WHERE n.group_id = %s", $group_id);
+    $query_string = sprintf("SELECT n.group_id, n.header_id, n.db_version, h.mjd, h.ra_deg, h.dec_deg FROM noshows as n LEFT JOIN headers as h ON n.header_id=h.header_id AND n.db_version=h.db_version WHERE n.group_id = %s", $group_id);
     
     $stmt = $db->prepare($query_string);
     $result = $stmt->execute();
